e0f596e79797f98382bdbfb40d6c3f0949c35ccf
Asher <124173983+99lash@users.noreply.github.com>
2023-11-24 19:21:33 +0800

    Updated dashboard and main
    +dashboard
     -accounts login & signup
     -implements structs & file handling
    +main
     -stage and level under construction

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Case-study-in-C
-This is a case study 
+MATH WHIZ
+{
+developed and program by COLMO and MANIT.
+}
+
+----
+ABOUT
+    This console application challenge users to solve random mathematical questions and different operations (dynamic questions) according to the designated difficulty of current stage and level of the user. 
+
+    -It has 6 stages and 5 levels in every stage. 
+
+    -Each stage has its own difficulty which depends on mathematical operators, and increasing limits of given numbers every level up.
+
+    -Finish the game to gain “MATH WHIZ” title on the Leaderboards
+----
+LEVEL?
+    User need to answer the given mathematical question correctly in order to proceed on the next level.
+----
+STAGE?
+    User need to finish all levels in a stage in order to proceed on next stage.
+----
+LOG IN
+    User need to be log-in on account to play Math Whiz. To do that, just simply select “[2] LOG IN” on screen of LANDING page.
+----
+SIGN UP
+    If user don’t have an account, you can create one by simply selecting “[1] SIGN UP” on screen of LANDING page.
+----
+MENU
+    Once log in successfully, user will be re-direct to “MENU PAGE”, where user can choose whether to START playing MATH WHIZ or VIEW LEADERBOARDS.
+----
+LEADERBOARDS
+    User can view leaderboards by simply selecting
+    “[2] LEADERBOARDS” on MENU page.
+----
+START
+    User can start playing by simply selecting “[1] START” on
+    MENU page.
+----
---
+++ dashboard.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+//#include <time.h> wala pang math whiz main
+
+struct user{
+    char username[20];
+    char password [12];
+};
+
+struct user_progress{
+    int stage;
+    int level;
+};
+
+void title()
+{
+printf("Math Whiz");
+printf("\n\n\n\n\n");
+}
+
+int main()
+{
+
+    int select, decision=0;
+    char confirm_password[12], iUsername[20], iPassword[12];
+    FILE *f1;
+    struct user account;
+
+    dashboard:
+    do{
+    title();
+    printf("\n");
+    printf("[1] CREATE\n");
+    printf("[2] LOG IN \n");
+    printf("[3] ABOUT MATH WHIZ\n");
+    printf("[4] EXIT");
+    printf("\n\n\n\n\n");
+    printf("Select :\t");
+    scanf("%i", &select);
+    
+    switch(select){
+        case 1 : //REGISTER
+
+        system("CLS");
+        retry_loop:
+        f1 = fopen("account_details.txt", "a+");
+        printf("\nEnter preferred username:\t");
+        scanf("%s", &account.username);
+        printf("\nEnter preferred password:\t");
+        scanf("%s", &account.password);
+        printf("\nRe-enter preferred password:\t");
+        scanf("%s", &confirm_password);
+        if(!strcmp(account.password, confirm_password)){
+            printf("Account Created\n");
+            fwrite(&account, sizeof(struct user), 1, f1);
+            //fprintf(f1, "%s", &account.username);
+            //fprintf(f1, "%s\n", &account.password);
+        }
+        else{
+            system("CLS");
+            goto retry_loop;
+        }
+        fclose(f1);
+        getch();
+        system("CLS");
+        break;
+
+        case 2: //LOG IN
+
+        system("CLS");
+        printf("Username:\t");
+        scanf("%s", &iUsername);
+        printf("Password:\t");
+        scanf("%s", &iPassword);
+        f1 = fopen("account_details.txt", "a+");
+        while(fread(&account, sizeof(struct user),1, f1)!=EOF){
+        //while(fscanf(f1, "%s", &account.username)!=EOF){
+        //fscanf(f1, "%s", &account.password);
+            if(!strcmp(account.username, iUsername)){
+                if(!strcmp(account.password, iPassword)){
+                printf("A1");
+                decision=1;
+                break;
+                }
+                else
+                    printf("A");
+                break;
+            }
+            else{
+                printf("B");
+                goto dashboard;
+            }
+        }
+        if(decision != 0){
+        printf("\nLOGIN SUCCESSFUL\nClick any key to continue");
+        getch();
+        decision = 0;
+        select = 5;
+        fclose(f1);
+        system("CLS");
+        }
+        else if(decision == 0){
+        printf("\nINVALID CREDENTIALS\nClick any key to continue");
+        fclose(f1);
+        getch();
+        system("CLS");
+        }
+        else{
+        printf("SOMETHING WENT ERROR!");
+        fclose(f1);
+        getch();
+        system("CLS");
+        }
+        break;
+
+        case 3: //ABOUT MATH WHIZ
+        system("CLS");
+        printf("MATH WHIZ ABOUT");
+        printf("\n\n\n\n\n\n");
+        printf("PRESS ANY KEY TO GO BACK");
+        getch();
+        system("CLS");
+        goto dashboard;
+    }
+    }while(select!=4);
+return 0;
+}
