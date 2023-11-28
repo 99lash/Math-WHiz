5e7841d95e55fb281567b511bdf4d4ee1efddd00
Asher <124173983+99lash@users.noreply.github.com>
2023-11-29 01:33:30 +0800

    Ito lang nakayanan sa ngayon. Good Night.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <conio.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 /* 
 --------------GUIDELINE AND OUTLINE OF PROGRAM-------------
 --DASHBOARD{
@@ -44,97 +45,138 @@ struct user
 //SIGNUP FUNCTION//
 int signup(int isRegistered)
 {   
-    char confirm_password[12];
+    char option, username_checker[20], confirm_password[12], file_name[FILENAME_MAX];
     struct user account;
     FILE *details_handler;
     isRegistered = 0;
 
-    details_handler = fopen("account_details.txt","a+");
-    do{
-    if(details_handler == NULL) printf("ERROR_FILE_NOT_FOUND_3"); 
+    tryagain:
     system("cls");
     printf("CREATE YOUR ACCOUNT");
     printf("\n\n\n");
     printf("Enter your username:\t");
-    scanf("%s", &account.username);
+    scanf("%s", &username_checker);
     printf("Enter your password:\t");
     scanf("%s", &account.password);
     printf("Confirm your passsword:\t");
     scanf("%s", &confirm_password);
-        if(!strcmp(account.password, confirm_password)){
-        printf("\nAccount has been created successfully\nPress any key to continue");
-        fwrite(&account, sizeof(struct user),1,details_handler);
-        fclose(details_handler);    
+    strcpy(file_name, username_checker); 
+
+    details_handler = fopen(strcat(file_name,".txt"),"w+");
+    if(details_handler == NULL) printf("ERROR_FILE_NOT_FOUND_3");
+    if(!strcmp(account.username, username_checker)){
+        printf("\nUSERNAME HAVE ALREADY EXIST!\n");
+        fclose(details_handler);
         getch();
-        break;
+        return isRegistered;
+        
+    }
+    else{
+        if(!strcmp(account.password, confirm_password)){
+            fwrite(&account, sizeof(struct user),1,details_handler);
+            fclose(details_handler);
+            isRegistered=1;
+            printf("\nAccount has been created successfully\nPress any key to continue");
+            getch();
         }
         else{
-        printf("\nPassword doesn't match\n");
-        getch();
+            printf("\nPASSWORD DOES NOT MATCH\n\nPress Y to try again\nPress B to go back to dashboard");
+            option = getch();
+            if(option == 121 || option == 89 ){
+                goto tryagain;
+            }
+            else if(option == 98 || option == 66){
+                return isRegistered = 0;
+            }
         }
-        }while(strcmp(account.password, confirm_password));
+    }
 
-return isRegistered = 1;
+return isRegistered;
 }//END OF SIGNUP FUNCTION//
 
 //LOGIN FUNCTION//
 int login(int isLogin)
 {
+    
     isLogin=0;
-    int find_user=0;
-    char username_checker[20], password_checker[12];
+    int isUsername_exist, isPassword_exist;
+    char username_checker[20], password_checker[12], file_name[FILENAME_MAX];
     struct user account;
-    FILE *details_checker;
+    FILE *details_handler;
 
-    details_checker = fopen("account_details.txt", "r");
-    if(details_checker == NULL) printf("ERROR_FILE_NOT_FOUND_3");
-    do{
     printf("LOGIN INTO YOUR ACCOUNT");
     printf("\n\n\n");
     printf("Username:\t");
     scanf("%s", username_checker);
     printf("Password:\t");
     scanf("%s", password_checker);
-    //GANON PARIN PROBLEMA SA ACCOUNT LOGIN: AUTHENTICATION ISSUE OF USERNAME AND PASSWORD IDENTIFIER SA FILE. 
-    while(fread(&account, sizeof(struct user),1,details_checker)!=EOF){
-        if(!strcmp(account.username, username_checker)){
+    strcpy(file_name,username_checker);
+    details_handler = fopen(strcat(file_name,".txt"), "r");
+    if(details_handler == NULL) printf("\nACCOUNT DOES NOT EXIST\n");
+    else{
+        while(fread(&account, sizeof(struct user),1, details_handler)!=EOF){
             if(!strcmp(account.password, password_checker)){
-            printf("\nLOGIN SUCCESS\nPress any key to continue\n");
-            getch();
+            isLogin = 1;
             break;
             }
             else{
-            printf("\nINVALID PASSWORD");
-            getch();
+            isLogin=0;
             break;
             }
-        find_user=1;
         }
-        if(find_user!=1){
-        printf("\nUser is not yet registered\n");
-        break;
-        }   
+        fclose(details_handler);
     }
-    }while(find_user!=1);
-    fclose(details_checker);
-return isLogin=1;
+return isLogin;
 }
 //END OF LOGIN FUNCTION//
 
-void menu()
+int menu(char menu_option)
 {
+    tryagain:
     printf("MATH WHIZ");
     printf("\n\n\n");
     printf("[G] START\n");
     printf("[L] LEADERBOARDS\n");
     printf("[A] ABOUT\n");
     printf("[X] EXIT\n");
+    menu_option = getch();
+    menu_option = toupper(menu_option);
+    switch(menu_option){
+        
+        case 71 : printf("G: THIS IS MATH WHIZ PAGE");
+        break;
+        
+        case 76 : printf("L: THIS IS LEADERBOARDS PAGE");
+        break;
+        
+        case 65 : printf("A: THIS IS ABOUT PAGE");
+        about(menu_option);
+        break;
+        
+        case 88 : printf("X: THIS EXIT BUTTON");
+        break;
+        
+        default : system("cls"); goto tryagain;
+    }
+return menu_option;
+}
+
+int about(char  about_option)
+{   
+    printf("MATH WHIZ ABOUT");
+    printf("\n\n\n\n\n\n");
+    printf("Press [B] to go back to menu");
+    about_option = getch();
+    about_option = toupper( about_option);
+    if(about_option == 66){
+    return  about_option;
+    }
 }
 
 int main()
 {   
-    int option_select, isRegistered = 0, isLogin = 0;
-    char confirm_passsword[12];
+    int dashboard_select, isRegistered = 0, auth = 0;
+    char confirm_passsword[12], menu_option;
     struct user account;
 
     //FILE *details_handler;
@@ -145,29 +187,35 @@ int main()
     printf("[2] SIGN UP\n");
     printf("[3] EXIT\n\n\n");
     printf("SELECT:\t");
-    scanf("%i", &option_select);
+    scanf("%i", &dashboard_select);
     //END OF DASHBOARD PAGE//
-    switch(option_select){
+    switch(dashboard_select){
         case 1 : //LOG IN PAGE FUNCTION//
         system("cls");
-        isLogin = login(isLogin);
+        auth = login(auth);
+         if(auth==0){
+        printf("Press any key to continue");
+        getch();
+        system("cls");
+        goto dashboard;
+        }
+        else if(auth == 1){
+        system("cls");
+        menu(menu_option);
+        }
         break;//END LOG IN PAGE FUNCTION//
         
         case 2 : //SIGN UP PAGE FUNCTION//
         system("cls");
         isRegistered = signup(isRegistered);
+        if(isRegistered==1){
+        system("cls");
+        goto dashboard;
+        }
         break;//END OF SIGN UP PAGE FUNCTION//
 
         case 3 : //EXIT CODE
         return 1;
-    }
-    if(isRegistered==1){
-        system("cls");
-        goto dashboard;
-    }
-    if(isLogin==1){
-        system("cls");
-        menu();
     }
 return 0;
 }
