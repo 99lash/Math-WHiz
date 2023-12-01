7c5d9bb3f34777d9996ac1eceb55f2cc9dae255f
Asher <124173983+99lash@users.noreply.github.com>
2023-12-02 00:24:22 +0800

    Update dec 2

--- main.c
+++ main.c
@@ -103,11 +103,10 @@ int signup(int isRegistered)
 //LOGIN FUNCTION//
 int login(int isLogin)
 {
-    
-    isLogin=0;
     char username_checker[20], password_checker[12], file_name[FILENAME_MAX];
     struct user account;
     FILE *details_handler;
+    isLogin=0;
 
     printf("LOGIN INTO YOUR ACCOUNT");
     printf("\n\n\n");
@@ -162,20 +161,29 @@ char about(char about_option){
 }//END OF ABOUT FUNCTION//
 */
 
+void leaderboards(){
+    system("cls");
+    printf("LEADERBOARDS\n\n\n");
+    printf("-----------------------------\n");
+    printf("| NO | NAME | STAGE & LEVEL |");
+    printf("\n-----------------------------");
+}
+
 int main()
 {   
     int isRegistered = 0, auth = 0;
-    char dashboard_option, confirm_passsword[12], menu_option, about_option;
+    char dashboard_option, confirm_passsword[12], menu_option, about_option, buffer[510];
     struct user account;
+    FILE *about;
 
     //FILE *details_handler;
-    //DASHBOARD PAGE//
+    //DASHBOARD PAGE---------------------------/
     dashboard:
     printf("MATH WHIZ\n\n\n");
     printf("[1] LOG IN\n[2] SIGN UP\n[3] EXIT\n\n\n");
     printf("PRESS THE CORRESPONDING KEY TO SELECT");
     dashboard_option = getch();
-    //END OF DASHBOARD PAGE//
+    //END OF DASHBOARD PAGE-----------------/-/
     if(dashboard_option == 49){//LOG IN PAGE FUNCTION // ASCII CODE OF [1] == 49
         system("cls");
         auth = login(auth);
@@ -183,18 +191,18 @@ int main()
             goto dashboard;
         }
         else if(auth == 1){ 
-            system("cls"); //MENU PAGE//
+            system("cls"); //MENU PAGE-----------------/
             while(menu_option != 71 && menu_option != 76 && menu_option != 65 && menu_option != 88){ // di ko maexplain and imagine kung bakit gumagana && instead of ||. BAKA PAGOD NA UTAK
                 system("cls"); 
                 printf("MATH WHIZ");
                 printf("\n\n\n");
-                printf("[G] START\n[L] LEADERBOARDS\n[A] ABOUT PAGE\n[X] EXIT");
+                printf("[G] START\n[L] LEADERBOARDS\n[A] ABOUT PAGE\n[X] LOG OUT & EXIT");
                 menu_option = getch();
                 menu_option = toupper(menu_option);
-                //END OF MENU PAGE //
+                //END OF MENU PAGE-----------------/-/
 
                 switch(menu_option){
-                    case 71 : //MATH WHIZ GAME//
+                    case 71 : //MATH WHIZ GAME---------------/
                         do{ //ASCII CODE OF [B] == 66
                             system("cls");
                             printf("MATH WHIZ GAME\n\n\n");
@@ -204,39 +212,47 @@ int main()
                             if(about_option == 66) break;
                         }while(about_option != 66);
                         menu_option = 00; break;
-                        //MATH WHIZ PAGE//
+                        //MATH WHIZ PAGE-----------------/-/
                     
-                    case 76 : //LEADERBOARDS PAGE//
+                    case 76 : //LEADERBOARDS PAGE-------------------/
                         do{ //ASCII CODE OF [B] == 66
-                            system("cls");
-                            printf("LEADERBOARDS\n\n\n");
-                            printf("PRESS [B] TO GO BACK TO MENU\n");
+                            leaderboards();
+                            printf("\n\n\nPRESS [B] TO GO BACK TO MENU\n");
                             about_option = getch();
                             about_option = toupper(about_option);
                             if(about_option == 66) break;
                         }while(about_option != 66);
                         menu_option = 00; break;
-                        //END OF LEADERBOARDS PAGE//
+                        //END OF LEADERBOARDS PAGE-----------------/-/
 
-                    case 65 : //ABOUT PAGE//
+                    case 65 : //ABOUT PAGE-------------------/
                         do{ //ASCII CODE OF [B] == 66
                             system("cls");
-                            printf("THIS IS ABOUT MATH WHIZ\n\n\n");
-                            printf("PRESS [B] TO GO BACK TO MENU\n");
+                            about = fopen("about.txt","r");
+                            if(about == NULL) printf("ERROR_FILE_MISSING_3");
+                            else{
+                                while(fgets(buffer, 510, about)!=NULL){
+                                    printf("%s", buffer);
+                                }
+                            }
+                            fclose(about);
+                            //printf("\tMath whiz is not only a game; it's an adventure through stage and level to solve random\n mathematical questions that puts your intuition and critical thinking to the test. This game\n console promises a delightful user experience for all users.\n\n\n");
+                            //printf("--MADE BY COLMO & ASHER MANIT--");
+                            printf("\n\n\nPRESS [B] TO GO BACK TO MENU\n");
                             about_option = getch();
                             about_option = toupper(about_option);
                             if(about_option == 66) break;
                         }while(about_option != 66);
                         menu_option = 00; break;
-                        //END OF ABOUT PAGE//
+                        //END OF ABOUT PAGE-----------------/-/
 
                     case 88 : exit(0);
                 }
             }   
         }
-    }//END LOG IN PAGE FUNCTION//
+    }//END LOG IN PAGE FUNCTION-----------------/-/
     else if(dashboard_option == 50){ // // ASCII CODE OF [2] == 50
-        //SIGN UP PAGE FUNCTION//
+        //SIGN UP PAGE FUNCTION-----------------/
         system("cls");
         isRegistered = signup(auth);
         if(isRegistered==1){
@@ -246,11 +262,11 @@ int main()
         else if(isRegistered==-1){
             system("cls");
             goto dashboard;
-        }//END OF SIGN UP PAGE FUNCTION//
+        }//END OF SIGN UP PAGE FUNCTION-------------/-/
     }
     else if(dashboard_option == 51){ // ASCII CODE OF [3] == 51
         //EXIT CODE
-        return 1;
+        exit(0);
     }
     else{
         printf("\nINVALID KEY SELECTION\n");
@@ -260,3 +276,40 @@ int main()
     }
 return 0;
 }
+
+/*
+-URGENT TODAY, DEC 1.-
+
+[IMPLEMENT] FILE HANDLING FOR USERS PROGRESS
+1. in Math Whiz (need to save or store the current stage & level of user - into users own file)
+2. in Leaderboards (need to display the all of the users current stage & level in a SORTED WAY)
+
+-SET ASIDE MUNA- 
+
+[BUG] FILE HANDLING FOR USERS ACCOUNT IN SIGNUP FUNCTION
+
+1. Can't detect or determine if the username already exist when creating a new account. - This could lead to overwriting files.
+
+    EXAMPLE;
+
+    there's an account name USER1 which is nasa stage 5 na ang progress.
+
+    then may nag create ng account at na pinangalanang USER1 ang username.
+
+    yung progress ng USER1 ay mababalik completely sa zero, dahil na overwrite yung USER1 account.
+
+2. It automatically creates empty file_name even if the sign up is unsuccessful.
+
+    EXAMPLE;
+
+    username: USER1
+    password: 123
+    confirm password: 123123df
+
+    PASSWORD DOESN'T MATCH - [SIGN UP WAS UNSUCCESSFUL]
+    ---------------------------------------------------
+    FILE CREATED
+
+    USER1.txt
+    ---------------------------------------------------
+*/
