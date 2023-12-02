6286823148a63b4f3102817dc545b8c6298d0371
Asher <124173983+99lash@users.noreply.github.com>
2023-12-03 02:33:54 +0800

    UPDATE 2:33 AM DEC 3, 2023

--- main.c
+++ main.c
@@ -1,315 +1,320 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <conio.h>
-#include <string.h>
-#include <time.h>
 #include <ctype.h>
-/* 
---------------GUIDELINE AND OUTLINE OF PROGRAM-------------
---DASHBOARD{
-    [1] LOGIN PAGE
-    [2] SIGN UP PAGE
-    [3] EXIT 
-}
+#include <time.h>
 
---MENU{
-    [G] START MATH WHIZ
-    [L] LEADERBOARDS
-    [A] ABOUT MATH WHIZ
-    [X] EXIT OPTION
-}
+int counter=0;
 
---ABOUT MATH WHIZ{
-    [B] BACK TO MENU OPTION
-}
-
---LEADERBOARDS{
-    [B] BACK TO MENU OPTION
-    [X] EXIT OPTION
-}
-
---MATH WHIZ{
-
-    [X] EXIT OPTION
-}
------------------------------------------------------------
-*/
-
-struct user
-{
-    char username[20];
-    char password[12];
-    struct progress{
-        int stage, level;
-    }mode[2];
+struct user{
+    char username[20], password[12];
+    int stage;
 };
+//PROTOTYPE FUNCTIONS OF SYSTEM OUTLINE/GUIDELINES FLOW
+void signup();
+void login();
+void menu();
+void math_whiz();
+void about();
+void leaderboards();
 
-//SIGNUP FUNCTION//
-int signup(int isRegistered)
-{   
-    char option, username_checker[20], confirm_password[12], file_name[20];
-    struct user account;
-    FILE *details_handler;
-    isRegistered = 0;
+//Function of Operators
+int addition(int n1, int n2) {return n1 + n2;}
+int subtraction(int n1, int n2) { return n1 - n2; }
+int multiplication(int n1, int n2) { return n1 * n2; }
+float division(float n1, float n2) { return n1 / n2; }
+//Function of Mixed Operators
+int addandsub(int n1, int n2, int n3) { return n1 + n2 - n3; }
+float mulanddiv(float n1, float n2, float n3) { return n1 * n2 / n3; }
+float allOp(float n1, float n2, float n3, float n4, float n5) {return n1 * n2 / n3 + n4 - n5;}
 
-    tryagain:
-    system("cls");
-    printf("CREATE YOUR ACCOUNT");
-    printf("\n\n\n");
-    printf("Enter your username:\t");
-    scanf("%s", &username_checker);
-    printf("Enter your password:\t");
-    scanf("%s", &account.password);
-    printf("Confirm your passsword:\t");
-    scanf("%s", &confirm_password);
-    strcpy(file_name, username_checker);
-    details_handler = fopen(strcat(file_name,".txt"),"w");
-    if(details_handler == NULL) printf("ERROR_FILE_NOT_FOUND_3");
-    if(!strcmp(account.username, username_checker)){
-        printf("\nUSERNAME HAVE ALREADY EXIST!\n");
-        fclose(details_handler);
-        getch();
-        return isRegistered;
-        
+
+//Correct or Wrong Identifier
+void identifier(int a, int b){
+    if(a == b){ 
+    printf("KORIQUE!!!");
     }
-    else{
-        if(!strcmp(account.password, confirm_password)){
-            fwrite(&account, sizeof(struct user),1,details_handler);
-            fclose(details_handler);
-            printf("\nAccount has been created successfully\nPress any key to continue");
-            getch();
-            return isRegistered=1;
-        }
-        else{
-            A:
-            printf("\nPASSWORD DOES NOT MATCH\n\n\nPRESS [Y] TO TRY AGAIN \nPRESS [N] TO GO BACK TO DASHBOARD");
-            option = getch();
-            option = toupper(option);
-            if(option == 89){ // ASCII CODE OF [Y] == 89
-                system("cls");
-                goto tryagain;
-            }
-            else if(option == 78){ // ASCII CODE OF [N] == 66
-                return isRegistered-1;
-            }
-            else{
-                system("cls");
-                goto A;
-            }
-        }
+    else if(a != b){
+    printf("ENGKK!!");
+    counter++;
     }
-}//END OF SIGNUP FUNCTION//
-
-//LOGIN FUNCTION//
-int login(int isLogin)
-{
-    char username_checker[20], password_checker[12], file_name[FILENAME_MAX];
-    struct user account;
-    FILE *details_handler;
-    isLogin=0;
-
-    printf("LOGIN INTO YOUR ACCOUNT");
-    printf("\n\n\n");
-    printf("Username:\t");
-    scanf("%s", username_checker);
-    printf("Password:\t");
-    scanf("%s", password_checker);
-    strcpy(file_name,username_checker);
-    details_handler = fopen(strcat(file_name,".txt"), "r");
-    if(details_handler == NULL) { //find the account name using file_name
-        printf("\nACCOUNT DOES NOT EXIST\n");
-        fclose(details_handler);
-        getch();
-        system("cls");
-        return isLogin = 0;
-    } 
-    else{
-        while(fread(&account, sizeof(struct user),1, details_handler)!=EOF){
-            if(!strcmp(account.password, password_checker)){
-            isLogin = 1;
-            break;
-            }
-            else{
-            printf("\nINCORRECT PASSWORD");
-            isLogin=0;
-            getch();
-            system("cls");
-            break;
-            }
-        }
-        fclose(details_handler);
-    }
-return isLogin;
-}
-//END OF LOGIN FUNCTION//
-
-//MENU FUNCTION//
-
-    
-//END OF MENU FUNCTION
-/*
-//ABOUT FUNCTION
-char about(char about_option){
-    if(about_option != 66){ //ASCII CODE OF [B] == 66
-        system("cls");
-        printf("THIS IS ABOUT MATH WHIZ\n\n\n");
-        printf("PRESS [B] TO GO BACK TO MENU\n");
-        about_option = getch();
-        about_option = toupper(about_option);
-    }
-    else return about_option;
-}//END OF ABOUT FUNCTION//
-*/
-
-void leaderboards(){
-    system("cls");
-    printf("LEADERBOARDS\n\n\n");
-    printf("-----------------------------\n");
-    printf("| NO | NAME | STAGE & LEVEL |");
-    printf("\n-----------------------------");
 }
 
-int main()
-{   
-    int isRegistered = 0, auth = 0;
-    char dashboard_option, confirm_passsword[12], menu_option, about_option, buffer[510];
-    struct user account;
-    FILE *about;
+//Prototype Function of Stages
+void stage1();
+void stage2();
+void stage3();
+void stage4();
+void stage5();
+void final_stage();
 
-    //FILE *details_handler;
-    //DASHBOARD PAGE---------------------------/
-    dashboard:
+
+int main(){
+    char option;
+    //dashboard//
+    while(option != '1' && option != '2' && option != '3'){ //ASCII CODE [1] is 49, [2] is 50, [3] is 51//
     printf("MATH WHIZ\n\n\n");
-    printf("[1] LOG IN\n[2] SIGN UP\n[3] EXIT\n\n\n");
-    printf("PRESS THE CORRESPONDING KEY TO SELECT");
-    dashboard_option = getch();
-    //END OF DASHBOARD PAGE-----------------/-/
-    if(dashboard_option == 49){//LOG IN PAGE FUNCTION // ASCII CODE OF [1] == 49
-        system("cls");
-        auth = login(auth);
-        if(auth==0){
-            goto dashboard;
+    printf("[1] LOG IN\n[2] SIGN UP\n[3] EXIT\n");
+    option = getch();
+    option = toupper(option);
+        switch(option){
+            case '1' : system("cls"); login(); break;
+            case '2' : system("cls"); signup(); break;
+            case '3' : system("cls");  break;
+            default  : printf("\nINVALID KEY SELECTION\n"); getch(); system("cls"); continue;
         }
-        else if(auth == 1){ 
-            system("cls"); //MENU PAGE-----------------/
-            while(menu_option != 71 && menu_option != 76 && menu_option != 65 && menu_option != 88){ // di ko maexplain and imagine kung bakit gumagana && instead of ||. BAKA PAGOD NA UTAK
-                system("cls"); 
-                printf("MATH WHIZ");
-                printf("\n\n\n");
-                printf("[G] START\n[L] LEADERBOARDS\n[A] ABOUT PAGE\n[X] LOG OUT & EXIT");
-                menu_option = getch();
-                menu_option = toupper(menu_option);
-                //END OF MENU PAGE-----------------/-/
-
-                switch(menu_option){
-                    case 71 : //MATH WHIZ GAME---------------/
-                        do{ //ASCII CODE OF [B] == 66
-                            system("cls");
-                            printf("MATH WHIZ GAME\n\n\n");
-                            printf("PRESS [B] TO GO BACK TO MENU\n");
-                            about_option = getch();
-                            about_option = toupper(about_option);
-                            if(about_option == 66) break;
-                        }while(about_option != 66);
-                        menu_option = 00; break;
-                        //MATH WHIZ PAGE-----------------/-/
-                    
-                    case 76 : //LEADERBOARDS PAGE-------------------/
-                        do{ //ASCII CODE OF [B] == 66
-                            leaderboards();
-                            printf("\n\n\nPRESS [B] TO GO BACK TO MENU\n");
-                            about_option = getch();
-                            about_option = toupper(about_option);
-                            if(about_option == 66) break;
-                        }while(about_option != 66);
-                        menu_option = 00; break;
-                        //END OF LEADERBOARDS PAGE-----------------/-/
-
-                    case 65 : //ABOUT PAGE-------------------/
-                        do{ //ASCII CODE OF [B] == 66
-                            system("cls");
-                            about = fopen("about.txt","r");
-                            if(about == NULL) printf("ERROR_FILE_MISSING_3");
-                            else{
-                                while(fgets(buffer, 510, about)!=NULL){
-                                    printf("%s", buffer);
-                                }
-                            }
-                            fclose(about);
-                            //printf("\tMath whiz is not only a game; it's an adventure through stage and level to solve random\n mathematical questions that puts your intuition and critical thinking to the test. This game\n console promises a delightful user experience for all users.\n\n\n");
-                            //printf("--MADE BY COLMO & ASHER MANIT--");
-                            printf("\n\n\nPRESS [B] TO GO BACK TO MENU\n");
-                            about_option = getch();
-                            about_option = toupper(about_option);
-                            if(about_option == 66) break;
-                        }while(about_option != 66);
-                        menu_option = 00; break;
-                        //END OF ABOUT PAGE-----------------/-/
-
-                    case 88 : exit(0);
-                }
-            }   
-        }
-    }//END LOG IN PAGE FUNCTION-----------------/-/
-    else if(dashboard_option == 50){ // // ASCII CODE OF [2] == 50
-        //SIGN UP PAGE FUNCTION-----------------/
-        system("cls");
-        isRegistered = signup(auth);
-        if(isRegistered==1){
-            system("cls");
-            goto dashboard;
-        }
-        else if(isRegistered==-1){
-            system("cls");
-            goto dashboard;
-        }//END OF SIGN UP PAGE FUNCTION-------------/-/
-    }
-    else if(dashboard_option == 51){ // ASCII CODE OF [3] == 51
-        //EXIT CODE
-        exit(0);
-    }
-    else{
-        printf("\nINVALID KEY SELECTION\n");
-        getch();
-        system("cls");
-        goto dashboard;
     }
 return 0;
 }
+//SIGNUP FUNCTION//
+void signup(){
+    int tmp=0;
+    char username_checker[20], password_checker[12], confirm_password[12], file_name[20], option;
+    struct user account;
+    FILE *f1;
+
+    do{
+        printf("SIGN UP\n\n\n");
+        printf("\nEnter preferred username:\t");
+        scanf("%s", username_checker);
+        printf("\nEnter preferred password:\t");
+        scanf("%s", confirm_password);
+        printf("\nRepeat preferred password:\t");
+        scanf("%s", password_checker);
+        strcpy(file_name, username_checker);
+        if(!strcmp(password_checker, confirm_password)){
+            strcpy(account.password,password_checker);
+            f1 = fopen(strcat(file_name,".dat"),"w+");
+            fwrite(&account,sizeof(struct user),1,f1);
+            printf("\nSIGN UP SUCCESS\nPRESS ANY KEY TO CONTINUE");
+            fclose(f1);
+            getch();
+            tmp=1;
+            system("cls");
+            login();
+        }
+        else{
+        //ascii code of [ESC] is 27
+        while(option != 27){ 
+            printf("\nPASSWORD DOES NOT MATCH\n\nPress any key to sign up again.\nPress ESC to go back to dashboard\n");
+            option = getch();
+            if(option == 27){ 
+                tmp=1;
+                break;
+            }else{
+                break;
+            }
+        system("cls");
+        }
+        }
+    system("cls");
+    }while(tmp!=1);
+main(); 
+}//END OF SIGNUP FUNCTION//
+
+//LOGIN//
+void login(){
+    char username_checker[20], password_checker[12], file_name[20];
+    struct user account;
+    FILE *f1;
+    printf("LOG IN\n\n\n");
+    printf("\nEnter username:\t");
+    scanf("%s", username_checker);
+    printf("\nEnter password:\t");
+    scanf("%s", password_checker);
+    strcpy(file_name,username_checker);
+    f1 = fopen(strcat(file_name,".dat"),"r");
+    if(f1 == NULL){
+        fclose(f1);
+        printf("\nUSER DOES NOT EXIST");
+        getch();
+        system("cls");
+        main();
+    }
+    else{
+        while(fread(&account, sizeof(struct user),1,f1)!=EOF){
+            if(!strcmp(account.password,password_checker)){
+                fclose(f1);
+                system("cls");
+                menu();
+                break;
+            }
+            else{
+                fclose(f1);
+                printf("\nINCORRECT PASSWORD");
+                getch();
+                system("cls");
+                main();
+                break;
+            }
+        }
+    }
+}//END OF LOGIN FUNCTION//
+
+//MENU FUNCTION//
+void menu(){
+    int tmp=0;
+    char option;
+
+    do{
+        printf("MATH WHIZ\n\n\n");
+        printf("[P] START\n[L] LEADERBOARDS\n[A] ABOUT");
+        printf("\n\n\nPress ESC to log out and exit\t");
+        option = getch();
+        option = toupper(option);
+        switch(option){
+            case 'P' : //ascii code of [S] is 83//
+            math_whiz();
+            break;
+
+            case 'L' : //ascii code of [L] is 76//
+            system("cls");
+            leaderboards();
+            break;
+            case 'A' : //ascii code of [A] is 65//
+            system("cls");
+            about();
+            break;
+
+            default: system("cls");
+        } //ascii code of [ESC] is 27
+    }while(option!='S' && option!='L' && option!='P' && option!= 27); 
+    
+}//END OF MENU FUNCTION//
+
+void about(){
+    char option, buffer[255];
+    FILE *about;
+    do{
+    about = fopen("about1.txt", "r");
+    if(about == NULL) printf("ERROR_FILE_MISSING");
+    else{
+        while(fgets(buffer,255,about)!=NULL){
+        printf("%s", buffer);
+        }
+    }
+    fclose(about);
+    printf("Press ESC to go back");
+    option = getch();
+    option = toupper(option);
+    system("cls");
+    }while(option!=27); //ascii code of [ESC] is 27.
+    if(option == 27){
+        system("cls");
+        menu();
+    }
+}
+//LEADERBOARDS// ---DISPLAY LANG MUNA GUYS, KULANG PA SA LOGIC YUNG PROGRAMMER---
+void leaderboards(){
+    int i;
+    char option;
+    do{
+    printf("LEADERBOARDS\n\n\n");
+    for(i = 0; i<25; i++) printf("-");
+    printf("\n");
+    printf("| NO. | USERNAME | STAGE |");
+    printf("\n");
+    for(i = 0; i<25; i++) printf("-");
+    printf("\n---DISPLAY LANG MUNA GUYS, KULANG PA SA LOGIC YUNG PROGRAMMER---\n");
+    printf("\nPress ESC to go back");
+    option = getch();
+    option = toupper(option);
+    system("cls");
+    }while(option!=27); //ascii code of [ESC] is 27.
+    if(option==27){
+        system("cls");
+        menu();
+    }
+}//END OF LEADERBOARDS//
+
+//MATH WHIZ//
+void math_whiz(){
+    struct user account;
+    account.stage =0;
+    printf("\nMATH WHIZ NA GUYS");
+
+    if(account.stage == 0){
+        system("cls");
+        stage1();
+    }
+    // if(account.stage == 2){
+    //     stage2();
+    // }
+    // if(account.stage == 3){
+    //     stage3();
+    // }
+    // if(account.stage == 4){
+    //     stage4();
+    // }
+    // if(account.stage == 5){
+    //     stage5();
+    // }
+}//END OF MATH WHIZ//
 
 /*
--URGENT TODAY, DEC 1.-
 
-[IMPLEMENT] FILE HANDLING FOR USERS PROGRESS
-1. in Math Whiz (need to save or store the current stage & level of user - into users own file)
-2. in Leaderboards (need to display the all of the users current stage & level in a SORTED WAY)
+Stage 1
+level 1 correct 
+level 2 correct
+level 3 correct
+level 4 correct
+level 5 wrong
 
--SET ASIDE MUNA- 
+-YOU HAVE 1 WRONG ANSWERS-
 
-[BUG] FILE HANDLING FOR USERS ACCOUNT IN SIGNUP FUNCTION
+Press any key to try again.
+Press B to go back to dashboard
+Press ESC to log out and exit
+---------------------------------
 
-1. Can't detect or determine if the username already exist when creating a new account. - This could lead to overwriting files.
+Stage 1
+level 1 correct 
+level 2 correct
+level 3 correct
+level 4 correct
+level 5 correct
 
-    EXAMPLE;
+-YOU HAVE 0 WRONG ANSWERS-
 
-    there's an account name USER1 which is nasa stage 5 na ang progress.
+Press any key to continue to the next stage.
+Press B to go back to dashboard
+Press ESC to log out and exit
 
-    then may nag create ng account at na pinangalanang USER1 ang username.
-
-    yung progress ng USER1 ay mababalik completely sa zero, dahil na overwrite yung USER1 account.
-
-2. It automatically creates empty file_name even if the sign up is unsuccessful.
-
-    EXAMPLE;
-
-    username: USER1
-    password: 123
-    confirm password: 123123df
-
-    PASSWORD DOESN'T MATCH - [SIGN UP WAS UNSUCCESSFUL]
-    ---------------------------------------------------
-    FILE CREATED
-
-    USER1.txt
-    ---------------------------------------------------
 */
+
+//UNDER CONSTRUCTION PA ITO MUNA SA NGAYON, PINAPATULOG NA NI INA
+//TIME CHECK 1:44 AM, DEC 3, 2023
+
+void stage1(){
+    int n1, n2, sys_ans, user_ans, level=1;
+    srand(time(NULL));
+    printf("STAGE 1\n\n\n");
+    do{
+        if(level == 1){
+        n1 = (rand() % 25) + 1;
+        n2 = (rand() % 25) + 1;
+        printf("LEVEL %i\n",level);
+        sys_ans = addition(n1,n2);
+        printf("%i + %i = ", n1, n2);
+        scanf("%i", &user_ans);
+        identifier(user_ans, sys_ans);
+        printf("\nWRONG : %i\n\n", counter);
+        getch();
+        level++;    
+        }
+        else if(level == 2){
+        n1 = (rand() % 50) + 1;
+        n2 = (rand() % 50) + 1;
+        printf("LEVEL %i\n",level);
+        sys_ans = addition(n1,n2);
+        printf("%i + %i = ", n1, n2);
+        scanf("%i", &user_ans);
+        identifier(user_ans, sys_ans);
+        printf("\nWRONG : %i\n\n", counter);
+        getch();
+        level++;   
+        }
+    }while(level<=5);
+    
+}
