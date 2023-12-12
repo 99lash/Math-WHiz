46907f9c55cb9890ba1eaf001c7bc4a0b5b7f314
Asher <124173983+99lash@users.noreply.github.com>
2023-12-12 22:02:19 +0800

    Update ++ [Leaderboards added]

--- main.c
+++ main.c
@@ -15,6 +15,61 @@ void clrscr(void){
     system("cls");
 }
 
+int counter=0, level=1;
+char MW_OPTION, check_filename[20];
+const int stg_1=1, stg_2=2, stg_3=3, stg_4=4, stg_5=5, stg_godMode=6; 
+
+struct user{
+    char username[20], password[12];
+    int stage;
+}glob_acc;
+
+//PROTOTYPE FUNCTIONS OF SYSTEM OUTLINE/GUIDELINES FLOW
+void signup();
+void login();
+void menu();
+void math_whiz();
+void about();
+void leaderboards();
+
+//Prototype Function of Stages
+void stage1();
+void stage2();
+void stage3();
+void stage4();
+void stage5();
+void final_stage();
+
+//MAIN OPTION PROMPT
+void option_prompt();
+
+//Function of Operators
+int addition(int n1, int n2) {return n1 + n2;}
+int subtract(int n1, int n2) { return n1 - n2; }
+int multiply(int n1, int n2) { return n1 * n2; }
+float divide(float n1, float n2) { return n1 / n2; }
+//Function of Mixed Operators
+int add_sub(int n1, int n2, int n3) { return n1 + n2 - n3; }
+float mult_div(float n1, float n2, float n3) { return n1 * n2 / n3; }
+float all_op(float n1, float n2, float n3, float n4, float n5) {return n1 * n2 / n3 + n4 - n5;}
+
+//Correct or Wrong Identifier
+void identifier(int a, int b){//for int
+    if(a == b) printf("CORRECT!!!"); //COLOR GREEN 
+    else if(a != b){
+        printf("INCORRECT!!!"); //COLOR RED
+        counter++;
+    }
+}
+void fIdentifier(float a, float b){// for point decimals, [FLOAT]
+    if(a == b) printf("CORRECT!!!"); //COLOR GREEN
+    else if(a != b){
+        printf("INCORRECT!!!"); //COLOR RED
+        counter++;
+    }
+}
+
+//HIDE THE PASSWORD INPUT
 void hide_input(char password[12]){
 
 int i=0;
@@ -38,65 +93,11 @@ while(1){
 }
 }
 
-int counter=0, level=1;
-char MW_OPTION, check_filename[20];
-const int stg_1=1, stg_2=2, stg_3=3, stg_4=4, stg_5=5, stg_godMode=6; 
-
-struct user{
-    char username[20], password[12];
-    int stage;
-}glob_acc;
-
-//PROTOTYPE FUNCTIONS OF SYSTEM OUTLINE/GUIDELINES FLOW
-void signup();
-void login();
-void menu();
-void math_whiz();
-void about();
-void leaderboards();
-
-//Function of Operators
-int addition(int n1, int n2) {return n1 + n2;}
-int subtract(int n1, int n2) { return n1 - n2; }
-int multiply(int n1, int n2) { return n1 * n2; }
-float divide(float n1, float n2) { return n1 / n2; }
-//Function of Mixed Operators
-int add_sub(int n1, int n2, int n3) { return n1 + n2 - n3; }
-float mult_div(float n1, float n2, float n3) { return n1 * n2 / n3; }
-float all_op(float n1, float n2, float n3, float n4, float n5) {return n1 * n2 / n3 + n4 - n5;}
-
-//Correct or Wrong Identifier
-void identifier(int a, int b){//for int
-    if(a == b) printf("KORIQUE!!!"); 
-    else if(a != b){
-        printf("ENGKK!!");
-        counter++;
-    }
-}
-void fIdentifier(float a, float b){// for point decimals, [FLOAT]
-    if(a == b) printf("KORIQUE!!!");
-    else if(a != b){
-        printf("ENGKK!!");
-        counter++;
-    }
-}
-
-//Prototype Function of Stages
-void stage1();
-void stage2();
-void stage3();
-void stage4();
-void stage5();
-void final_stage();
-
-//MAIN OPTION PROMPT
-void option_prompt();
-
 int main(){
     char option;
     clrscr();
     //dashboard//
-    while(option != '1' && option != '2' && option != 27){ //ASCII CODE [1] is 49, [2] is 50, [3] is 51//
+    while(option != '1' && option != '2' && option != ESC){ //ASCII CODE [1] is 49, [2] is 50, [3] is 51//
     printf("MATH WHIZ\n\n\n");
     printf("[1] LOG IN\n[2] SIGN UP\n");
     printf("\n\n\nPRESS ESC TO EXIT\n");
@@ -104,7 +105,7 @@ int main(){
         switch(option){
             case '1' : clrscr(); login(); break;
             case '2' : clrscr(); signup(); break;
-            case 27  : clrscr();  break;
+            case ESC : clrscr();  break;
             default  : printf("\nINVALID KEY SELECTION\n"); getch();
             clrscr(); break;
         }
@@ -118,7 +119,7 @@ void signup(){
     clrscr();
     char username_checker[20], password_checker[12], confirm_password[12], file_name[20], option;
     struct user loc_acc;
-    FILE *fw, *fr;
+    FILE *fw, *fr, *lb;
 
     do{
         printf("SIGN UP\n\n\n");
@@ -137,20 +138,21 @@ void signup(){
             fclose(fr);
             if(!strcmp(password_checker, confirm_password)){
                 strcpy(loc_acc.password,confirm_password);
-                //strcpy(acc.username,file_name);
+                //strcpy(loc_acc.username,username_checker);
                 loc_acc.stage = stg_1;
                 //printf("\n%s", file_name); getch();
                 fw = fopen(file_name,"w");
-                //fprintf(f1,"%s\t%i\n",acc.password); //WRITE TO NG STRING SA FILE HEHE 
-                fwrite(&loc_acc,sizeof(struct user),1,fw);    // PANG WRITE  LANG NG BINARY CANCEL MUNA  
-                printf("\nSIGN UP SUCCESS\nPRESS ANY KEY TO CONTINUE");
+                fwrite(&loc_acc,sizeof(struct user),1,fw);
                 fclose(fw);
-                
-                fw = fopen("leaderboards.txt","a");
-                fprintf(fw, "%s\t%i\n",loc_acc.username,loc_acc.stage);
-                fclose(fw); 
-                option = 00; getch(); clrscr();
-                login();
+                //---------------------------------
+                lb = fopen("leaderboards.txt","a");
+                strcpy(glob_acc.username,username_checker);
+                glob_acc.stage = stg_1;
+                fwrite(&glob_acc,sizeof(struct user),1,lb);
+                fclose(lb);
+                fflush(stdin);
+                printf("\nSIGN UP SUCCESS\nPRESS ANY KEY TO CONTINUE");
+                option = 00; getch(); clrscr(); login();
             }
             else{//ascii code of [ESC] is 27
                 while(option!=ESC){ 
@@ -166,7 +168,6 @@ void signup(){
             printf("\nUSERNAME ALREADY EXIST"); getch();
             fclose(fr); 
         }
-        clrscr();
     }while(option!=00);
     main(); 
 }//END OF SIGNUP FUNCTION//
@@ -195,6 +196,7 @@ void login(){
         //(fscanf(f1,"%s", account.password)!=EOF){
         while(fread(&glob_acc, sizeof(struct user),1,fr)!=EOF){ //READ BINARY FROM FILE
             if(!strcmp(glob_acc.password,password_checker)){
+            strcpy(glob_acc.password,password_checker);
             fclose(fr);
             clrscr(); menu(); break;
             }
@@ -265,7 +267,9 @@ void about(){
 //LEADERBOARDS// ---DISPLAY LANG MUNA GUYS, KULANG PA SA LOGIC YUNG PROGRAMMER---
 void leaderboards(){
     int i;
-    char option;
+    char option,buffer[255];
+    struct user loc_acc;
+    FILE *fr;
     do{
         printf("LEADERBOARDS\n\n\n");
         for(i = 0; i<25; i++) printf("-");
@@ -273,7 +277,12 @@ void leaderboards(){
         printf("| NO. | USERNAME | STAGE |");
         printf("\n");
         for(i = 0; i<25; i++) printf("-");
-        printf("\n---DISPLAY LANG MUNA GUYS, KULANG PA SA LOGIC YUNG PROGRAMMER---\n");
+        fr = fopen("leaderboards.txt", "r");
+        while(fread(&loc_acc, sizeof(struct user),1,fr)){
+            printf("\n\t%s", loc_acc.username);
+            printf("\t%7i", loc_acc.stage);
+        }
+        fclose(fr);
         printf("\nPress ESC to go back");
         option = getch();
         option = toupper(option);
@@ -285,20 +294,16 @@ void leaderboards(){
 
 //MATH WHIZ//
 void math_whiz(){
-    //int stage_1=1, stage_2=2, stage_3=3, stage_4=4;
-    char ign[20];
     struct user loc_acc;
     FILE *fr;
-     
-    //printf("%i\n", account.stage); getch(); //wag pansinin wala to. 
-    //strcpy(ign,check_filename);
+
     fr = fopen(check_filename,"r");
     if(fr == NULL){
         printf("ERROR OPENING FILE");
         fclose(fr); exit(0);
     }
     else{
-        while(fread(&loc_acc, sizeof(struct user),1,fr)!=EOF){
+        while(fread(&loc_acc, sizeof(struct user),1,fr)){
         //while(fscanf(f1,"%i",acc.stage)!=EOF){
             if(loc_acc.stage == stg_1){
                 //printf("%i\n", loc_acc.stage); getch(); 
@@ -325,21 +330,12 @@ void math_whiz(){
                 clrscr(); final_stage(); break;
             }
             else{
-                printf("ERROR ON LOADING OF LOADED PROGRESS"); getch(); exit(0); 
+                printf("ERROR ON ALLOCATING OF LOADED PROGRESS"); 
+                getch(); exit(0); 
             }
         }
     }
     fclose(fr); 
-
-    // if(account.stage == 3){
-    //     stage3();
-    // }
-    // if(account.stage == 4){
-    //     stage4();
-    // }
-    // if(account.stage == 5){
-    //     stage5();
-    // }
 }//END OF MATH WHIZ//
 
 void option_prompt(){
@@ -384,11 +380,17 @@ void option_prompt(){
 }
 
 void stage1(){ //ADDITION & SUBTRACTION
-    int n1, n2, sys_ans, user_ans;
-    FILE *fw;
+    int n1, n2, sys_ans, user_ans,found=0;
+    struct user loc_acc;
+    FILE *fw,*lb;
+    char ign[20];
 
+    strcpy(ign,glob_acc.username);
+    //strcpy(loc_acc.username,ign);
     srand(time(NULL));
-    //printf("%s", check_filename);
+    //printf("\n%s", check_filename); .dat
+    //printf("\n%s", loc_acc.username);
+    //printf("\n%s", ign);
     while(MW_OPTION != ESC && MW_OPTION != 'B'){
         printf("STAGE 1\n");
         do{
@@ -423,12 +425,32 @@ void stage1(){ //ADDITION & SUBTRACTION
     }
     else{
         glob_acc.stage = stg_2;
-        getch();
-        //fprintf(f1, "\n%i", account.stage);
         fwrite(&glob_acc, sizeof(struct user),1,fw);
         fclose(fw);
     }
-    stage2();
+    lb = fopen("leaderboards.txt", "r");
+    while(fread(&glob_acc,sizeof(struct user),1,lb)){
+        if(!strcmp(glob_acc.username,ign)){ 
+        found=1;
+        }
+    }
+    fclose(lb);
+
+    if(found==1){
+        fw = fopen(check_filename,"r");
+        lb = fopen("leaderboards.txt","a");
+        while(fread(&glob_acc,sizeof(struct user),1,fw)){
+            fwrite(&glob_acc,sizeof(struct user),1,lb);
+        }
+        fclose(fw);
+        fclose(lb);
+    }
+    else {
+        printf("\nUSER NOT FOUND");
+        fclose(fw);
+        fclose(lb);
+    }
+stage2();
 }
 
 void stage2(){ //MULTIPLICATION
